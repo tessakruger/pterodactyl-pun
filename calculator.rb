3477e073437978a6f5b6b4e2ecaa1183b3e1c711
tessakruger <tessarfoto@gmail.com>
2016-01-05 01:31:49 -0500

    Re-add file

---
+++ calculator.rb
@@ -0,0 +1,13 @@
+class Calculator  
+  def add(num_1, num_2)
+    num_1 + num_2
+  end
+
+  def subtract(num_1, num_2)
+    num_1 - num_2
+  end
+<<<<<<< HEAD
+end
+=======
+end
+>>>>>>> 0140150c5dbe05289faa4ae6555ca8701b9a15bd
